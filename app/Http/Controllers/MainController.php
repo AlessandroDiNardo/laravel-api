24f6018fdeff29f7db18d2f23596425682df8953
AlessandroDiNardo <alexdna003@gmail.com>
2023-02-15 17:28:28 +0100

    install bootstrap

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Tag;
+use App\Models\Genre;
+use App\Models\Movie;
+
+
+class MainController extends Controller
+{
+    // index
+    public function home(){
+        $genres = Genre :: all();
+        return view('pages.home', compact('genres'));
+    }
+
+}
