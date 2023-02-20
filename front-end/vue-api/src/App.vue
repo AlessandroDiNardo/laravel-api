1f43da9ac2d5df4083959cc49989714db6d304c4
AlessandroDiNardo <alexdna003@gmail.com>
2023-02-20 16:58:51 +0100

    test axios call to api movieAll

--- front-end/vue-api/src/App.vue
+++ front-end/vue-api/src/App.vue
@@ -1,47 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+
+import Axios from 'axios';
+
+export default {
+
+  mounted() {
+
+    Axios.get('http://localhost:8000/api/v1/movieAll')
+      .then(res => {
+
+        const data = res.data;
+        const success = data.success;
+        const movies = data.response;
+
+        console.log(movies);
+      })
+      .catch(err => console.error(err));
+  }
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Hello World!</h1>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
